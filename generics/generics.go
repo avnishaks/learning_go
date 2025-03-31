b46032d698f63ad29cdb42760c042811170437a7
avnishaks <avnishgolu927@gmail.com>
2025-04-01 03:00:39 +0530

    all the recent changes is added

--- generics/generics.go
+++ generics/generics.go
@@ -3,6 +3,6 @@ package generics
 import "fmt"
 
 func Generics() {
-	fmt.Printf("Hello welcome to world of generics how the world of generics works in Go Programming Languages")
+	fmt.Printf("\nHello welcome to world of generics how the world of generics works in Go Programming Languages")
 
 }
